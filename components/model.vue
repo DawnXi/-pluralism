af2eafc946ab39674ef2884e9384ed6f29f09fcf
DawnXi <31690897+DawnXi@users.noreply.github.com>
2019-04-30 16:32:30 +0800

    * 图片资源上传

---
+++ components/model.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="model-box">
+		<view class="model">
+			<view class="title">{{options.title}}</view>
+			<view class="content">{{options.content}}</view>
+			<view class="options">
+				<view class="ok">{{options.okText}}</view>
+				<view class="close">{{options.closeText}}</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		props:{
+			options: {
+				type: Object,
+				required: true
+			}
+		}	
+	}
+</script>
+
+<style lang="scss">
+
+</style>
